8d90a545097c295dd85e2e1030324d96fb1d7e3d
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-28 10:59:32 -0700

    Update title and tagline for docusaurus.config.js configuration.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,9 +5,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
-    tagline: 'Dinosaurs are cool',
-    url: 'https://your-docusaurus-test-site.com',
+    title: 'Docupotamus',
+    tagline: 'The fastest docs platform',
+    url: 'https://docupotamus.io',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
